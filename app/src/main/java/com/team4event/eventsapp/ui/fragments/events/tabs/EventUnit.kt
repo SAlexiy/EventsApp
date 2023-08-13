cdef57956ff3e09e50af286b626f4a8a43e67955
Alexey <91087810+AI-Kusu@users.noreply.github.com>
2023-08-13 12:34:52 +0500

    finally

--- app/src/main/java/com/team4event/eventsapp/ui/fragments/events/tabs/EventUnit.kt
+++ app/src/main/java/com/team4event/eventsapp/ui/fragments/events/tabs/EventUnit.kt
@@ -25,7 +25,7 @@ fun EventUnit(event: Event){
             .padding(vertical = 20.dp)
             ) {
             Box {
-                imageFromURL(event.img!!)
+                imageFromURL("event.img!!")
             }
 
             Column {
